10ad65e10d05c0fc3f95b2d13a0bb23b1649c7be
boba-milktea <catherine.idv@gmail.com>
2025-06-09 16:19:04 +0200

    improve ui

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -11,13 +11,13 @@ function CartOverview() {
   if (!totalQty) return null;
 
   return (
-    <div className="w-full flex p-6 text-lg lg:text-xl xl:text-2xl bg-link text-surface items-center justify-between font-bold">
+    <div className="w-full flex p-4 text-base lg:text-lg xl:text-xl bg-link text-surface items-center justify-between font-bold">
       <p className="flex gap-2">
         <span>{totalQty} books</span>
         <span> {formatCurrency(totalPrice)} </span>
       </p>
       <Link
-        className="flex text-lg lg:text-xl xl:text-2xl items-center hover:underline"
+        className="flex text-base lg:text-lg xl:text-xl items-center hover:underline"
         to="/cart"
       >
         Open Cart
